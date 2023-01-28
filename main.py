63a1f7f9dc6d82b21a7458037974e7bd5b9abca0
Erwin <erwin@getshitdone.at>
2023-01-28 21:01:51 +0100

    .env variables

--- main.py
+++ main.py
@@ -5,22 +5,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from decouple import config
 
+SERVER = config('SERVER')
+LOGIN= config('LOGIN')
+PASSWORD = config('PASSWORD')
+URL = config('URL')
 
 class StravaKudos:
 
     def __init__(self):
         self.options = webdriver.ChromeOptions()
-        self.driver = webdriver.Remote(command_executor=config('SELENIUMSERVER'),
+        self.driver = webdriver.Remote(command_executor=config(SERVER),
                                        options=self.options)
         self.driver.maximize_window()
 
     def load_page(self):
-        self.driver.get(config('URL'))
+        self.driver.get(URL)
         time.sleep(3)
         username = self.driver.find_element(By.NAME, "email")
         password = self.driver.find_element(By.NAME, "password")
-        username.send_keys(config('LOGIN'))
-        password.send_keys(config('PASSWORD'))
+        username.send_keys(LOGIN)
+        password.send_keys(PASSWORD)
         time.sleep(3)
         password.send_keys(Keys.RETURN)
 
