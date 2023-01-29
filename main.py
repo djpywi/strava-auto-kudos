50be9ac20e4fda1de885e1b38bbb11561212784f
Erwin <erwin@getshitdone.at>
2023-01-29 08:20:29 +0100

    start new webdriver session

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-SERVER = "http://10.10.1.38:4444"
+
+SERVER = "http://10.10.1.30:4444"
 LOGIN = "erwin@posteo.de"
 PASSWORD = """2%FAV'?>'-"H11:Els{;!!J`Crz*jTKm"""
 URL = "https://www.strava.com/dashboard/following/40"
@@ -32,9 +33,11 @@ class StravaKudos:
             self.driver.execute_script("document.querySelectorAll('[title=\"Give kudos\"]').forEach(button => button.click());")
             time.sleep(2)
             self.driver.execute_script("document.querySelectorAll('[title=\"Be the first to give kudos!\"]').forEach(button => button.click());")
+            self.driver.close()
             refresher = random.randint(1800, 4800)
             time.sleep(refresher)
-            self.driver.refresh()
+            self.load_page()
+            self.thumbs_up()
 
 
 kudos = StravaKudos()
